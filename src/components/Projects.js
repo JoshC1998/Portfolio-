c0b6c6395cd8c8d65be97220f0a0fd96b505d796
Joshua Cruz <jcruz1998@gmail.com>
2024-09-30 18:58:41 -0400

    deploy

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,21 +1,22 @@
 import {ProjectCard} from './ProjectCard'
 import galacticseige from '../assets/img/galactic_seige.gif'
+import PeakPerformance from '../assets/img/peak_performance.gif'
 import '../CSS/projectcard.css';
 
 
 export const Projects = () => {
     const projects = [
         {
-            title: "Galactic Seige",
+            title: "PeakPerformance",
             description: "a full-stack application providing personalized workout and meal planning solutions through dynamic RESTful APIs, secure user authentication, and AI-powered suggestions, with features for users to upload gym videos to be showcased on a leaderboard.",
-            imgUrl: galacticseige,
-            githubLink: "https://github.com/JoshC1998/Galactic_Seige",
+            imgUrl: PeakPerformance,
+            githubLink: "https://github.com/JoshC1998/PeakPerformance",
         },
         {
-            title: "PeakPerformance",
+            title: "Galactic Seiege",
             description: "a 2D game built with python and pygame, utiliziing the principles of object oriented programming",
             imgUrl: galacticseige,
-            githubLink: "https://github.com/JoshC1998/PeakPerformance",
+            githubLink: "https://github.com/JoshC1998/Galactic_Seige",
         },
     ]
     return (
