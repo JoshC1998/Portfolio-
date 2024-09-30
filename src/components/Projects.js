131500bc25bca723bc364d841500d2705ab0482e
Joshua Cruz <jcruz1998@gmail.com>
2024-09-30 15:19:52 -0400

    deploy

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,9 +7,15 @@ export const Projects = () => {
     const projects = [
         {
             title: "Galactic Seige",
-            description: "a 2D game built with python and pygame, utiliziing the principles of object oriented programming",
+            description: "a full-stack application providing personalized workout and meal planning solutions through dynamic RESTful APIs, secure user authentication, and AI-powered suggestions, with features for users to upload gym videos to be showcased on a leaderboard.",
             imgUrl: galacticseige,
             githubLink: "https://github.com/JoshC1998/Galactic_Seige",
+        },
+        {
+            title: "PeakPerformance",
+            description: "a 2D game built with python and pygame, utiliziing the principles of object oriented programming",
+            imgUrl: galacticseige,
+            githubLink: "https://github.com/JoshC1998/PeakPerformance",
         },
     ]
     return (
